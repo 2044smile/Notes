d8c6cbf2b98a3dce1c4ac63820d6393abc1c2e5a
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-06-03 19:47:55 +0900

    Fix: minor

---
+++ Python/전문가를_위한_파이썬/part3/dict&set/3.8 collective_theory.py
@@ -0,0 +1,15 @@
+# 집합(set) 이론
+## 집합(set)은 고유한 객체의 모음으로서, `기본적으로 중복 항목을 제거한다.`
+l = ['spam', 'eggs', 'spam', 'eggs']
+set(l)  # {'eggs', 'spam'}
+list(set(l))  # ['eggs', 'spam']
+
+## 집합 요소는 반드시 해시할 수 있어야 한다.
+## set은 해시 가능하지 않지만 frozenset은 해시 가능하므로, frozenset이 set에 들어갈 수 있다.
+
+# TMI
+## `list` 의 경우 hash 를 사용하지 않기 때문에 메모리를 적게 사용한다.
+### 그리고 구현 특성 상 list 가 단순하여 iteration(반복)이 더 빠르다. / 메모리 측면에서 list 가 적게 쓴다.
+### `list` 특히 `array list` 쓰는 것을 추천한다.
+## `set` 의 경우 전체를 한번씩 확인을 해야 한다 그래서 overhead 가 발생한다. 즉 iteration(반복) 이 느리다.
+### 메모리 사용 비용 증가
